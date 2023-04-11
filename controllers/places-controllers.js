6a1604e8e792336afae2247b055306949129b09a
Fahmi <itsfahmiefendy@gmail.com>
2023-04-12 00:54:28 +0700

    feat: delete place and remove DUMMY_PLACES

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -4,42 +4,7 @@ const Place = require("../models/place");
 const HttpError = require("../models/http-error");
 const addressToCoordinates = require("../utils/location");
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Borobudur",
-    description:
-      "Borobudur is a 9th-century Mahayana Buddhist temple in Magelang Regency, not far from the city of Magelang and the town of Muntilan, in Central Java, Indonesia",
-    imageUrl:
-      "https://img.okezone.com/content/2022/02/04/337/2542154/mengenal-raja-yang-meratakan-puncak-bukit-demi-membangun-candi-borobudur-thVuwzm6PE.jpg",
-    address:
-      "Jl. Badrawati, Kw. Borobudur Temple, Borobudur, Kec. Borobudur, Magelang Regency, Central Java",
-    coordinates: {
-      lat: -7.6060136,
-      lng: 110.1984,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "National Monument",
-    description:
-      "The National Monument is a 132 m (433 ft) obelisk in the centre of Merdeka Square, Central Jakarta, symbolizing the fight for Indonesia",
-    imageUrl:
-      "https://duitologi.com/media/5ZNKKORS9CTWC6NL1O3U74AXK7QT191G00NJNSNXQUKFVRJVSGPSUD9MNZD9RC4N.jpg.1250x660_q85.jpg",
-    address:
-      "RT.5/RW.2, Gambir, Gambir District, Central Jakarta City, Special Capital Region of Jakarta 10110",
-    coordinates: {
-      lat: -6.1753871,
-      lng: 106.8249588,
-    },
-    creator: "u2",
-  },
-];
-
 const getAllPlaces = async (req, res, next) => {
-  // res.json({ DUMMY_PLACES });
-
   let result;
 
   try {
@@ -60,7 +25,6 @@ const getAllPlaces = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  // const places = DUMMY_PLACES.filter((place) => place.creator === userId);
 
   let places;
 
@@ -96,7 +60,6 @@ const getPlacesByUserId = async (req, res, next) => {
 
 const getPlaceByPlaceId = async (req, res, next) => {
   const placeId = req.params.pid;
-  // const selectedPlace = DUMMY_PLACES.find((place) => place.id === placeId);
 
   let selectedPlace;
 
@@ -143,17 +106,6 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  // const newPlace = {
-  //   id: uuid.v4(),
-  //   title,
-  //   description,
-  //   address,
-  //   coordinates,
-  //   creator,
-  // };
-
-  // DUMMY_PLACES.push(newPlace);
-
   const newPlace = new Place({
     title,
     description,
@@ -188,17 +140,6 @@ const updatePlace = async (req, res, next) => {
 
   const { title, description } = req.body;
   const placeId = req.params.pid;
-
-  // const updatedPlace = {
-  //   ...DUMMY_PLACES.find((place) => place.id === placeId),
-  // };
-
-  // const placeIndex = DUMMY_PLACES.findIndex((place) => place.id === placeId);
-
-  // updatedPlace.title = title;
-  // updatedPlace.description = description;
-
-  // DUMMY_PLACES[placeIndex] = updatedPlace;
 
   let selectedPlace;
 
@@ -238,20 +179,41 @@ const updatePlace = async (req, res, next) => {
   });
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
 
-  const deletedPlace = DUMMY_PLACES.find((place) => place.id === placeId);
+  let selectedPlace;
 
-  if (!deletePlace) {
-    throw new HttpError(`Could not find a place with id of ${placeId}`, 404);
+  try {
+    selectedPlace = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      `Failed to get a place with id of ${placeId} because of ${err.message}`,
+      500
+    );
+    return next(error);
   }
 
-  DUMMY_PLACES = DUMMY_PLACES.filter((place) => place.id !== placeId);
+  if (!selectedPlace) {
+    const error = new HttpError(`There is no place with id of ${placeId}`, 404);
+    // const error = new Error(`Could not find place with id of ${placeId}`);
+    // error.code = 404;
+    return next(error);
+  }
+
+  try {
+    await selectedPlace.deleteOne();
+  } catch (err) {
+    const error = new HttpError(
+      `Failed to delete a place with id of ${placeId} because of ${err.message}`,
+      500
+    );
+    return next(error);
+  }
 
   res
     .status(200)
-    .json({ message: "Successfully deleted a data!", data: deletedPlace });
+    .json({ message: "Successfully deleted a data!", data: selectedPlace });
 };
 
 exports.getAllPlaces = getAllPlaces;
